a330cc8853ad9e71860527fd84d49cad9ca48b7c
hy-rf <heyu880202@gmail.com>
2025-08-29 23:46:04 +0800

    feat: add locale changer

---
+++ client/app/components/setting/LocaleChanger.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { locales, setLocale } = useI18n();
+</script>
+
+<template>
+  <button v-for="locale in locales" @click="setLocale(locale.code)">
+    <p>{{ locale.name }}</p>
+  </button>
+</template>
+
+<style lang="css" scoped></style>
