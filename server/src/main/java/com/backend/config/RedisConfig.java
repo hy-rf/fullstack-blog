a414459f5ef0e31fb6dd0a770fe28e082c631261
hy-rf <heyu880202@gmail.com>
2025-08-30 20:57:24 +0800

    chore: remove redis

--- server/src/main/java/com/backend/config/RedisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories(basePackages = "com.backend.repository.cache")
-public class RedisConfig {
-}
