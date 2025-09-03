53582b1d3e14e8f278d3f126c6303b1010012818
hy-rf <heyu880202@gmail.com>
2025-09-03 21:15:36 +0800

    feat: added delete saved post

--- client/app/pages/me/saved-posts.vue
+++ client/app/pages/me/saved-posts.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import type PostSummary from '~/types/PostSummary';
-
+const { t } = useI18n()
 const { data, pending } = useFetch<PostSummary[]>("/api/saved-posts")
-
+const postsToShow = ref(data)
+async function deleteSavedPost(postId:number) {
+  const r = await fetch("/api/saved-post" + "?postId=" + postId, {
+    method: "delete"
+  })
+  if (r.ok) {
+    postsToShow.value=postsToShow.value?.filter(e=>e.id!=postId)!
+  }
+}
 </script>
 
 <template>
-  <PostCard v-for="post in data" :post="post" />
+  <div>
+    <div v-for="post in postsToShow" >
+      <PostCard :post="post" />
+      <button @click="deleteSavedPost(post.id)">{{ t("me.saved.delete_button") }}</button>
+    </div>
+  </div>
   <div v-if="pending">Loading</div>
 </template>
 
