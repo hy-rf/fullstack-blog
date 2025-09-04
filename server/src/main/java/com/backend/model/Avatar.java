57765b063591b4d5691933da75b0b7203d7a6d9c
hy-rf <heyu880202@gmail.com>
2025-09-04 22:17:46 +0800

    chore: added Avatar and PostImage entities

---
+++ server/src/main/java/com/backend/model/Avatar.java
@@ -0,0 +1,18 @@
+package com.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "avatars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Avatar {
+
+  Integer userId;
+  String url;
+}
