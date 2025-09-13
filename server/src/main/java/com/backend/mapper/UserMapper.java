35bc5fef0c867e94dd5a4e7e7d2fe7cf5e4e997e
hy-rf <heyu880202@gmail.com>
2025-09-13 23:31:11 +0800

    chore: added select users with limit and offset

--- server/src/main/java/com/backend/mapper/UserMapper.java
+++ server/src/main/java/com/backend/mapper/UserMapper.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMapper {
   UserBasicDto selectBasicById(Integer id);
+  UserBasicDto selectAll(Integer offset, Integer limit);
 }
