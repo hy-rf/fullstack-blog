32e854b683771efc71c98e601025034605713661
hy-rf <heyu880202@gmail.com>
2025-09-14 20:43:22 +0800

    chore: code cleanup

--- server/src/main/java/com/backend/repository/PostRepository.java
+++ server/src/main/java/com/backend/repository/PostRepository.java
@@ -6,12 +6,8 @@ import com.backend.repository.dto.PostPage;
 import com.backend.repository.dto.SavedPost;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
@@ -20,20 +16,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface PostRepository
-  extends JpaRepository<Post, Integer>, JpaSpecificationExecutor<Post> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
   @Override
   @NonNull
   @EntityGraph(attributePaths = { "author" })
   Optional<Post> findById(@NonNull Integer id);
-
-  @Override
-  @NonNull
-  @EntityGraph(attributePaths = { "author" })
-  Page<Post> findAll(
-    @Nullable Specification<Post> spec,
-    @NonNull Pageable pageable
-  );
 
   /**
    * Get feed posts on home page
