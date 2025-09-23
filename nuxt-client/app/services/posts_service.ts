a028d8d176da4191a67458d9a1de88a2f24bab57
hy-rf <heyu880202@gmail.com>
2025-09-23 15:31:20 +0800

    chore: call different url prefix for server and client side

--- nuxt-client/app/services/posts_service.ts
+++ nuxt-client/app/services/posts_service.ts
@@ -2,9 +2,10 @@ import type PostList from "~/types/PostList";
 import type SearchQuery from "~/types/SearchQuery";
 
 export async function fetchPosts(params: SearchQuery) {
-  console.log(params.authorName);
-
-  return await $fetch<PostList>(`/api/posts/search`, {
+  const url = import.meta.server
+    ? `${useRuntimeConfig().URL}/posts/search`
+    : `${useRuntimeConfig().public.GATEWAY_URL}/posts/search`;
+  return await $fetch<PostList>(url, {
     query: params,
     credentials: "include",
   });
