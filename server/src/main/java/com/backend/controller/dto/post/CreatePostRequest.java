2b2ce12d2bba3b2d0e1f7433903dd7dce3e00e40
hy-rf <heyu880202@gmail.com>
2025-09-24 15:53:21 +0800

    chore: update CreatePostRequest to include tags and imagesBase64Strings

--- server/src/main/java/com/backend/controller/dto/post/CreatePostRequest.java
+++ server/src/main/java/com/backend/controller/dto/post/CreatePostRequest.java
@@ -1,5 +1,6 @@
 package com.backend.controller.dto.post;
 
+import java.util.List;
 import java.util.Optional;
 import lombok.Data;
 
@@ -9,4 +10,6 @@ public class CreatePostRequest {
   private String content;
   private Optional<Integer> rootPostId = Optional.empty();
   private Optional<Integer> postId = Optional.empty();
+  private List<String> imagesBase64Strings;
+  private List<String> tags;
 }
