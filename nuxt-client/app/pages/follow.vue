decd88d6f67ceb080bf73659e2c836e20e3a4a7f
hy-rf <heyu880202@gmail.com>
2025-10-07 14:45:28 +0800

    feat: fetch following

--- nuxt-client/app/pages/follow.vue
+++ nuxt-client/app/pages/follow.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 const { t } = useI18n();
+const config = useRuntimeConfig();
+
+let data;
+if (import.meta.server) {
+  const headers = useRequestHeaders(["cookie"]);
+  data = await $fetch(`${config.URL}/following`, {
+    credentials: "include",
+    headers,
+  });
+} else {
+  data = await $fetch(`${config.public.GATEWAY_URL}/following`, {
+    credentials: "include",
+  });
+}
 </script>
 
 <template>
   <div>
     <h1 class="title">{{ t("follow.title") }}</h1>
     <div>follow</div>
+    {{ data.length }}
   </div>
 </template>
 
