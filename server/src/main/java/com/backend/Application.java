a74a05cd4b3afb951dc9dcb25c70bd2add6dc9cf
hy-rf <heyu880202@gmail.com>
2025-10-07 14:45:40 +0800

    chore: enable async

--- server/src/main/java/com/backend/Application.java
+++ server/src/main/java/com/backend/Application.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = { "com.backend" })
 @EnableJpaRepositories(basePackages = "com.backend.repository")
+@EnableAsync
 public class Application {
 
   @PostConstruct
