09f22f1d2c1eca099a9460a4876bbc7a8555c7c0
hy-rf <heyu880202@gmail.com>
2025-10-25 14:51:35 +0800

    chore: remove unused classes

--- server/src/main/java/com/backend/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.repository;
-
-import com.backend.repository.dto.SaveFileResult;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository {
-  public SaveFileResult saveFile();
-}
--- server/src/main/java/com/backend/repository/FileRepositoryConfig.java
+++
@@ -1,6 +0,0 @@
-package com.backend.repository;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FileRepositoryConfig {}
