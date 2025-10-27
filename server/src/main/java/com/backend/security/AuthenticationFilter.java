0ffa10da01a6c018f28826bdc06e62223e9b195e
hy-rf <heyu880202@gmail.com>
2025-10-27 16:15:41 +0800

    chore: code cleanup

--- server/src/main/java/com/backend/security/AuthenticationFilter.java
+++ server/src/main/java/com/backend/security/AuthenticationFilter.java
@@ -14,7 +14,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
